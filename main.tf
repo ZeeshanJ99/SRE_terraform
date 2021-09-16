41e1a149c970397bc21964a5cb1ee9a4ed597c5b
ZeeshanJ99 <88186084+ZeeshanJ99@users.noreply.github.com>
2021-09-16 11:00:12 +0100

    main.tf

--- main.tf
+++ main.tf
@@ -4,3 +4,22 @@
 provider "aws" {
   region = "eu-west-1"
 }
+
+
+# Lets start with Launching an EC2 instance using the app AMI
+
+resource "aws_instance" "app_instance" {
+ ami = "ami-00cce017f5ffc9635"
+ instance_type = "t2.micro"
+ associate_public_ip_address = true
+ tags = {
+   Name = "sre_zeeshan_terraform_app" 
+ }
+
+}
+
+# ami id ` `
+# `sre_key.pem` file
+# AWS keys setup is already done
+# public ip
+# type of the instance `t2micro`
